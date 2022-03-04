2f541f7100c5c4bed705cb845bcefbdae4a01dae
Dari Alves <darieltondf@gmail.com>
2022-03-03 22:10:14 -0300

    updated

---
+++ 220211PROADM02/aula-1/src/index.js
@@ -0,0 +1,182 @@
+// var nomeDoAluno = 'Dari'
+
+// let idadeDoAluno = 36
+
+// idadeDoAluno = 42
+
+// console.log(idadeDoAluno + 50)
+
+// let nome = 'Dari'
+// let valor = 36.50
+// let solteiro = true
+
+// // typeof
+
+// // console.log(typeof nome);
+// // console.log(typeof idade);
+// // console.log(typeof valor);
+// // console.log(typeof solteiro);
+// let idade = 36
+// console.log(typeof idade);
+// console.log(typeof '36');
+
+// console.log(idade == '36')
+// console.log(idade === '36')
+
+// console.log(idade === '36')
+
+// console.log(valor1 >= valor2);
+
+// //  false  || true = true
+// // OU
+// console.log(true || false);
+// console.log(false || false);
+
+// E
+// console.log(false && true);
+// console.log(true && true);
+// maior ou igual
+
+// let valor1 = 50
+// let valor2 = 5
+
+// let resultado = (valor1 + valor2) / 10
+// // let resultado2 = valor1 - valor2
+// // let resultado3 = valor1 * valor2
+// // let resultado4 = valor1 / valor2
+
+// console.log(resultado);
+// console.log(resultado2);
+// console.log(resultado3);
+// console.log(resultado4);
+
+// Módulo %
+
+// console.log(10 % 3);
+
+// let nota = 8
+
+// if (nota >= 7) {
+//     console.log('Aluno nota 10');
+// } else if(nota >= 5) {
+//     console.log('Aluno');
+// } else {
+//     console.log('Reprovado');
+// }
+
+// if (nota >= 5) {
+//     console.log('Aluno');
+// } else if(nota >= 7) {
+//     console.log('Aluno nota 10');
+// } else {
+//     console.log('Reprovado');
+// }
+
+// let nome = 'Jessica';
+
+// // concatenação
+// console.log("Bom dia," + " " + nome);
+
+// // Interpolação
+// console.log(`Bom dia, ${nome}`)
+
+// let nome = "Giselle"
+// let sobrenome = "Andrade"
+// let profissao = "Programadora"
+
+// let cartaoDeVisita = nome + " " + sobrenome + " - " + profissao;
+
+// let cartaoDeVisita = `${nome} ${sobrenome} === ${profissao}`
+
+// console.log(cartaoDeVisita);
+
+// let soma = `5 + 5 = ${5 + 5}`
+
+// console.log(soma);
+
+// let trabalhoDeHistoria = 0.0
+// let trabalhoDeMatematica = 7.0
+// let trabalhoDeCiencia = 0
+// let trabalhoDeGeografia = 9.5
+
+// let quantidadeDeTrabalhos = 4
+
+// let soma = trabalhoDeCiencia + trabalhoDeGeografia + trabalhoDeHistoria + trabalhoDeMatematica
+
+// let media = soma / quantidadeDeTrabalhos
+
+// console.log(`Média do aluno: ${media}`);
+
+// let trabalhoDeHistoria = 9.0
+// let trabalhoDeMatematica = 7.0
+// let trabalhoDeCiencia = 4
+// let trabalhoDeGeografia = 9.5
+
+// let quantidadeDeTrabalhos = 4
+
+// let soma = trabalhoDeCiencia + trabalhoDeGeografia + trabalhoDeHistoria + trabalhoDeMatematica
+
+// let media = soma / quantidadeDeTrabalhos
+
+// console.log(`Média do aluno: ${media}`);
+
+// let trabalhoDeHistoria = 9.0
+// let trabalhoDeMatematica = 7.0
+// let trabalhoDeCiencia = 4
+// let trabalhoDeGeografia = 9.5
+
+// let quantidadeDeTrabalhos = 4
+
+// let soma = trabalhoDeCiencia + trabalhoDeGeografia + trabalhoDeHistoria + trabalhoDeMatematica
+
+// let media = soma / quantidadeDeTrabalhos
+
+// console.log(`Média do aluno: ${media}`);
+
+/*
+    Manualmente
+*/
+
+// Manual
+/*
+    Entrada: Roupa 
+    Processamento: Lavar a roupa
+    Saída: Roupa Lavada
+*/
+
+// function maquinaDeLavar(roupa) {
+//     let roupaLavada = roupa + " lavada"
+
+//     return roupaLavada
+// }
+
+// let camiseta = "Camiseta Preta"
+// let bermuda = "Bermuda"
+
+// console.log(maquinaDeLavar(camiseta))
+
+// console.log(maquinaDeLavar(bermuda))
+
+// let trabalhoDeHistoria = 9.0
+// let trabalhoDeMatematica = 7.0
+// let trabalhoDeCiencia = 4
+// let trabalhoDeGeografia = 9.5
+
+// let quantidadeDeTrabalhos = 4
+
+// let soma = trabalhoDeCiencia + trabalhoDeGeografia + trabalhoDeHistoria + trabalhoDeMatematica
+
+// let media = soma / quantidadeDeTrabalhos
+
+// console.log(`Média do aluno: ${media}`);
+
+// Manual
+// function mediaDosAlunos(trabalho1, trabalho2, trabalho3, trabalho4) {
+//     let quantidadeDeTrabalhos = 4;
+//     let somaDosValores = trabalho1 + trabalho2 + trabalho3 + trabalho4
+//     let media = somaDosValores / quantidadeDeTrabalhos;
+
+//     return media
+// }
+
+// let resultado = mediaDosAlunos(9, 7, 6, 5)
